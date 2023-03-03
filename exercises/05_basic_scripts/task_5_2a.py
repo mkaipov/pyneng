2c2c60ba493f53f4ce1d29d9860fdf33ad06c6ed
mkaipov <mkkaipov@gmail.com>
2023-03-03 23:50:34 +0300

    Списал задание 5_2

--- exercises/05_basic_scripts/task_5_2a.py
+++ exercises/05_basic_scripts/task_5_2a.py
@@ -49,3 +49,31 @@ bin_ip = "00001010000000010000000111000011"
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+
+ip_address = input('Введите адрес сети: ')
+ip, mask = ip_address.split('/')
+ip_list = [int(i) for i in ip.split('.')]
+mask = int(mask)
+bin_mask = "1" * mask + '0' * (32 - mask)
+m1, m2, m3, m4 = [
+    int(bin_mask[0:8], 2),
+    int(bin_mask[8:16], 2),
+    int(bin_mask[16:24], 2),
+    int(bin_mask[24:32], 2),
+]
+
+
+ip_output = """
+Network:
+{0:<8}  {1:<8}  {2:<8}  {3:<8}
+{0:08b}  {1:08b}  {2:08b}  {3:08b}"""
+
+mask_output = """
+Mask:
+/{0}
+{1:<8}  {2:<8}  {3:<8}  {4:<8}
+{1:08b}  {2:08b}  {3:08b}  {4:08b}
+"""
+
+print(ip_output.format(ip_list[0], ip_list[1], ip_list[2], ip_list[3]))
+print(mask_output.format(mask, m1, m2, m3, m4))
