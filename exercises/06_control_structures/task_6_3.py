145c09720526f120d3134fc270aba7a126836fad
Murat Kaipov <mkkaipov@gmail.com>
2023-03-06 16:58:01 +0300

    Использовал re для удаления лишних символов

--- exercises/06_control_structures/task_6_3.py
+++ exercises/06_control_structures/task_6_3.py
@@ -51,21 +51,21 @@ interface FastEthernet0/7
 На стандартный поток вывода надо выводить только команды trunk настройки,
 а access закомментировать.
 """
-
+'''
 access_template = [
     "switchport mode access",
     "switchport access vlan",
     "spanning-tree portfast",
     "spanning-tree bpduguard enable",
 ]
-
+'''
 trunk_template = [
     "switchport trunk encapsulation dot1q",
     "switchport mode trunk",
     "switchport trunk allowed vlan",
 ]
 
-access = {"0/12": "10", "0/14": "11", "0/16": "17", "0/17": "150"}
+# access = {"0/12": "10", "0/14": "11", "0/16": "17", "0/17": "150"}
 trunk = {
     "0/1": ["add", "10", "20"],
     "0/2": ["only", "11", "30"],
@@ -82,3 +82,15 @@ trunk = {
 #         else:
 #             print(f" {command}")
 
+for intf, vlan in trunk.items():
+    print("interface FastEthernet" + intf)
+    for command in trunk_template:
+        if command.endswith('allowed vlan'):
+            if trunk.get(intf)[0] == 'add':
+                print(f" {command} {vlan[0]} {','.join(vlan[1::])}")
+            elif trunk.get(intf)[0] == 'only':
+                print(f" {command} {','.join(vlan[1::])}")
+            elif trunk.get(intf)[0] == 'del':
+                print(f" {command} {'remove'} {','.join(vlan[1::])}")
+        else:
+            print(f' {command}')
